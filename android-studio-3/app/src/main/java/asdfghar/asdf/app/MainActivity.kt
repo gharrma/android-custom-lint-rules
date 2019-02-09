8b146743f20968ca0f4cc6e5ebce21911e453497
Matthew Gharrity <gharrma@google.com>
2019-02-08 17:33:47 -0800

    Add app module for testing

---
+++ android-studio-3/app/src/main/java/asdfghar/asdf/app/MainActivity.kt
@@ -0,0 +1,13 @@
+package asdfghar.asdf.app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tmp = "hello, lint!"
+    }
+}
